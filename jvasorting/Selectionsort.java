4e948a0f13f5b6c3e2a9c540204d1fa14a1172aa
stech <sourabhgupta9509@gmail.com>
2023-12-17 15:12:16 +0530

    done

--- jvasorting/Selectionsort.java
+++ jvasorting/Selectionsort.java
@@ -1,24 +1,32 @@
-package jvasorting;
 
-public class Selectionsort {
-    
-
-    public static void main(String[] args) {
-        int arr[] = {9 , 3, 5 ,2 , 5, 7 ,1 , 1};
-        for( int i = 0 ; i< arr.length ; i ++){
-            int smallest = i;
-            for(int  j = i+1 ; i <arr.length ; j ++){
-                if(arr[smallest >arr[j]]){
-                    smallest = j;
-                }
-            }
-            int temp = arr[smallest];
-            arr[smallest] = arr[i];
-            arr[i] = temp;
+class Selectionsort{
+   public static void printArray(int arr[]) {
+       for(int i=0; i<arr.length; i++) {
+           System.out.print(arr[i]+" ");
+       }
+       System.out.println();
+   }
 
 
-        }
-            Print(arr);
+   public static void main(String args[]) {
+       int arr[] = {7, 8, 1, 3, 2};
 
-    }
+
+       //selection sort
+       for(int i=0; i<arr.length-1; i++) {
+           int smallest = i;
+           for(int j=i+1; j<arr.length; j++) {
+               if(arr[j] < arr[smallest]) {
+                   smallest = j;
+               }
+           }
+           //swap
+           int temp = arr[smallest];
+           arr[smallest] = arr[i];
+           arr[i] = temp;
+       }
+
+
+       printArray(arr);
+   }
 }
